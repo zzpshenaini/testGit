d6dc60f4af937844b385459492beb1e8794a04d6
tom_glb <goodMoring_glb@zzp.com>
2020-06-17 10:44:44 +0800

    zhangsanFirst

--- testGitDemo/src/main/java/com/atguigu/git/GitFirst.java
+++ testGitDemo/src/main/java/com/atguigu/git/GitFirst.java
@@ -5,5 +5,6 @@ public class GitFirst {
     public static void main(String[] args) {
         System.out.println("GitFirst...");
         System.out.println("Git操作02");
+        System.out.println("张三操作01");
     }
 }
