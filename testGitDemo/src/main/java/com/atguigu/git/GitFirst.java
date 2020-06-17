7bdc47f00771cb5ef74c8c188ddaf0d7d6bdec53
tom_glb <goodMoring_glb@zzp.com>
2020-06-17 11:34:44 +0800

    hot_fix,fix bug

--- testGitDemo/src/main/java/com/atguigu/git/GitFirst.java
+++ testGitDemo/src/main/java/com/atguigu/git/GitFirst.java
@@ -7,5 +7,7 @@ public class GitFirst {
         System.out.println("Git操作02");
         System.out.println("master操作01");
         System.out.println("张三操作01");
+        System.out.println("修改Bug后...");
+        System.out.println("意外解决完毕");
     }
 }
