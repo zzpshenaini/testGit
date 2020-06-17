ea384b81b32e07bfc8e537d5a2dd3b25e5318069
tom_glb <goodMoring_glb@zzp.com>
2020-06-17 10:50:34 +0800

    masterFirst

--- testGitDemo/src/main/java/com/atguigu/git/GitFirst.java
+++ testGitDemo/src/main/java/com/atguigu/git/GitFirst.java
@@ -5,5 +5,6 @@ public class GitFirst {
     public static void main(String[] args) {
         System.out.println("GitFirst...");
         System.out.println("Git操作02");
+        System.out.println("master操作01");
     }
 }
