76fd562a7f9d91cc689bb814a7b08a17ab9fca13
tom_glb <goodMoring_glb@zzp.com>
2020-06-20 11:41:49 +0800

    测试

--- testGitDemo/src/main/java/com/atguigu/git/GitFirst.java
+++ testGitDemo/src/main/java/com/atguigu/git/GitFirst.java
@@ -8,9 +8,11 @@ public class GitFirst {
         System.out.println("master操作01");
         System.out.println("张三操作01");
         System.out.println("发生了紧急bug，需要先去改bug");
+
         System.out.println("意外发生....");
 
         System.out.println("修改Bug后...");
         System.out.println("意外解决完毕");
+      /*  System.out.println("意外解决完毕2");*/
     }
 }
